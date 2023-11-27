2c2aacae4e18ca2dbd8812c97310defdcd86567f
Onyoniken <onyonikenn@gmail.com>
2023-11-27 04:14:58 -0800

    100-write.py

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - main function
+ * @list: param 1
+ *
+ * Return: 1 on success , 0 on failure
+ */
+int check_cycle(listint_t *list)
+{
+        listint_t *a = list;
+        listint_t *b = list;
+
+        if (!list)
+                return (0);
+
+        while (a && b && b->next)
+        {
+                a = a->next;
+                b = b->next->next;
+                if (a == b)
+                        return (1);
+       }
+
+		return (0);
+}
