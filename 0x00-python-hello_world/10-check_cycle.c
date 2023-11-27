0335ccbd2a8cdb19d0a222d52155600b0ab75b8e
Onyoniken <onyonikenn@gmail.com>
2023-11-27 06:55:52 -0800

    10-check_cycle.c, lists.h

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,19 +8,18 @@
  */
 int check_cycle(listint_t *list)
 {
-        listint_t *a = list;
-        listint_t *b = list;
+	listint_t *a = list;
+	listint_t *b = list;
 
-        if (!list)
-                return (0);
-
-        while (a && b && b->next)
-        {
-                a = a->next;
-                b = b->next->next;
-                if (a == b)
-                        return (1);
-       }
-
+	if (!list)
 		return (0);
+
+	while (a && b && b->next)
+	{
+		a = a->next;
+		b = b->next->next;
+		if (a == b)
+		return (1);
+	}
+	return (0);
 }
