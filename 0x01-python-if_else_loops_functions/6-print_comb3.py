c4b8e966229e857904e4fd9aa23155f49360b091
Onyoniken <onyonikenn@gmail.com>
2023-11-28 07:30:51 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for k in range (0, 9):
     for l in range(k + 1, 10):
-        if k == 8 and l == 8:
+        if k == 8 and l == 9:
             print("{}{}".format(k, l))
         else:
-            print("{}{}".format(k, l), end =", ")
+            print("{}{}".format(k, l), end=", ")
