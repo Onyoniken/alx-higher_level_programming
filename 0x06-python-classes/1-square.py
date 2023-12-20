94009e53d550e7c2d4306269a0c280359b95fb3e
Onyoniken <onyonikenn@gmail.com>
2023-12-20 18:57:28 +0300

    1-square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""Squares a  module."""
 
 
 class Square:
+    """Defines a square to the module"""
 
     def __init__(self, size):
-
+        """Constructor.
+        Args:
+            size: length of the square.
+        """
         self.__size = size
+
