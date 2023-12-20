de73809f2bf4222c542c31f95dfca9fd57578a60
Onyoniken <onyonikenn@gmail.com>
2023-12-20 19:25:32 +0300

    vi 3-square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-# 0-square.py by Ehoneah Obed
+"""Defines Square."""
+
 
 class Square:
+    """Represents square."""
 
     def __init__(self, size=0):
-
+        """Initializes square.
+        Args:
+            size (int): The size of the square.
+        """
         if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
+        """Return the current area square."""
+        return (self.__size * self.__size)
 
-        return (self.__size ** 2)
