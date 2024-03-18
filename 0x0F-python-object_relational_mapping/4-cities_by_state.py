2fae12e6cf70bb15985d936a5a0b0f7b21d9b6f6
Onyoniken <onyonikenn@gmail.com>
2024-03-18 10:09:08 +0300

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,13 +10,13 @@ if __name__ == '__main__':
     """
     access database and gets cities
     """
-    db_connect = db.connect(host="localhost", port=3306, 
-            user=argv[1], passwd=argv[2], db=argv[3])
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
 
     with db_connect.cursor() as db_cursor:
-        db_cursor.execute("SELECT cities.id, cities.name, \ 
-                FROM cities JOIN states ON cities.state_id \= states.id ORDER BY cities.id ASC")
-
+        db_cursor.execute("SELECT cities.id, cities.name, states.name \
+                                FROM cities JOIN states ON cities.state_id \
+                                = states.id ORDER BY cities.id ASC")
         rows_selected = db_cursor.fetchall()
 
     if rows_selected is not None:
