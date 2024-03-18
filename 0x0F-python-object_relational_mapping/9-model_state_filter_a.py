01e6fc12c8ee1b1a08274e11877fc3a7b0ece681
Onyoniken <onyonikenn@gmail.com>
2024-03-18 12:53:47 +0300

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-script state object tha contains letter 'a'
+List containing State objects with letter `a`
+from `hbtn_0e_6_usa`.
 """
+
 from sys import argv
 from model_state import State, Base
 from sqlalchemy import create_engine
@@ -9,8 +11,10 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    states from database
+    Access to the database and gets states
+    from the database.
     """
+
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
 
