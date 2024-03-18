7ed3b3f1ca74c99c7c73f62f2e313d6025df9662
Onyoniken <onyonikenn@gmail.com>
2024-03-18 09:55:05 +0300

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyton3
+#!/usr/bin/python3
 
 """
 scripts lists from the database all states starting with letter "N"
@@ -10,15 +10,15 @@ from sys import argv
 gets states from database
 """
 if __name__ == '__main__':
-    db_connet = db.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-
-    db_cursor = db.connect.cursor()
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+    db_cursor = db_connect.cursor()
 
     db_cursor.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY 'N%' \ ORDER BY states.id ASC")
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC")
 
     rows_selected = db_cursor.fetchall()
 
     for row in rows_selected:
         print(row)
-
