a1a99603d2e6b9da4c95cbf4b8cb3b59077ac7fe
Onyoniken <onyonikenn@gmail.com>
2024-03-18 13:16:32 +0300

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-script changes name of state object from database
+This script changes the name of State object
+from the database `hbtn_0e_6_usa`.
 """
+
 from sys import argv
 from model_state import State, Base
 from sqlalchemy import create_engine
@@ -9,13 +11,14 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    update state objects from database
+    Updates State object on the database.
     """
+
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            argv[1], argv[2], argv[3])
+        argv[1], argv[2], argv[3])
 
     engine = create_engine(db_url)
-    Session =sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
 
     session = Session()
 
