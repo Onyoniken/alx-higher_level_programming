795e5429b4d8900731f80cd863111ff9c491ac24
Onyoniken <onyonikenn@gmail.com>
2024-03-18 10:02:58 +0300

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,20 +6,18 @@ script takes arg and displays values in states where name matches to arg in data
 import MySQLdb as db
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     """
     gets states from database
     """
     db_connect = db.connect(host="localhost", port=3306,
-            user=argv[1], passwd=argv[2], db=argv[3])
-
+                            user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db_connect.cursor()
 
     db_cursor.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \ 
-            states.id ASC", {'name': argv[4]})
-
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
+                        states.id ASC".format(argv[4]))
     rows_selected = db_cursor.fetchall()
 
     for row in rows_selected:
