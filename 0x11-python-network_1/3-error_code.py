e0ff8eba4582230ac0c8cb388668f788d16ac050
Onyoniken <onyonikenn@gmail.com>
2024-03-28 15:54:08 +0300

    0x11-python-network_1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-    """
-    Write a Python script that takes in a URL, 
-    sends a request to the URL and displays the 
-    body of the response (decoded in utf-8).
-    """
+"""
+Python script that takes in a URL, 
+sends a request to the URL and displays 
+the body of the response (decoded in utf-8).
+"""
 import sys
 import urllib.error
 import urllib.request
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
@@ -15,5 +16,5 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(request) as response:
             print(response.read().decode("ascii"))
-        except urllib.error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,12 @@
-#!/usr/bin/pyhon3
-    """
-    Python script that takes in a URL and an email address,
-    sends a POST request to the passed URL with the email as a parameter, 
-    and finally displays the body of the response.
-    """
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email address, 
+sends a POST request to the passed URL with the email 
+as a parameter, and finally displays the body of the response.
+"""
 import sys
 import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
